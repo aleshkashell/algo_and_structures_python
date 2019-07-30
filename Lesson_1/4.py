0968bc8bfb9b851ef2fde42717cedcd94ca1266b
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-07-30 14:55:25 +0300

    forth task done

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -8,3 +8,21 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+from random import random
+
+
+if __name__ == '__main__':
+    start = input('Введите начало диапазона: ')
+    end = input('Введите конец диапазона: ')
+
+    if start.isdigit() and end.isdigit():
+        print(int(random() * (int(end) - int(start) + 1) + int(start)))
+    elif start.isalpha() and end.isalpha():
+        print(chr(int(random() * (ord(end) - ord(start) + 1) + ord(start))))
+    else:
+        try:
+            m1 = float(start)
+            m2 = float(end)
+            print(round(random() * (m2 - m1) + m1, 3))
+        except ValueError:
+            print("Wrong numbers")
