b0398251ced666e070ab841c61c416079589371d
Aleksey <aleshkashell@gmail.com>
2019-08-04 19:52:26 +0300

    forth task done

--- Lesson_2/4.py
+++ Lesson_2/4.py
@@ -2,3 +2,15 @@
 4.	Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
 Количество элементов (n) вводится с клавиатуры.
 """
+
+
+def get_sum(count, divident=1):
+    if count == 0:
+        return 0
+    else:
+        return divident + get_sum(count-1, divident/2)
+
+
+if __name__ == '__main__':
+    num = int(input("Введите количестов элементов последовательности: "))
+    print(f'Сумма последовательности: {get_sum(num)}')
