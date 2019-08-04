0c31784c150bb0f4bbd93adae933de92bb530ef2
Aleksey <aleshkashell@gmail.com>
2019-08-04 19:45:34 +0300

    third task done

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,3 +3,16 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+
+
+def reverse_print(number):
+    if number < 10:
+        print(number, end='')
+    else:
+        print(number % 10, end='')
+        reverse_print(number // 10)
+
+
+if __name__ == '__main__':
+    num = int(input("Введите число: "))
+    reverse_print(num)
