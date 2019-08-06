f0380a9882e8cdf200295583c098f32302a39bac
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-08-06 14:22:37 +0300

    eighth task done

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,26 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+from random import randint
+
+
+def generate_array(count=20):
+    random_array = [0] * count
+    for i in range(0, len(random_array)):
+        random_array[i] = randint(-50, 50)
+    return random_array
+
+
+if __name__ == '__main__':
+    # input matrix
+    matrix = [generate_array(4) for _ in range(4)]
+    # count matrix
+    matrix.append([0] * 4)
+    for i in range(0, 4):
+        for j in range(0, 4):
+            matrix[4][i] = matrix[4][i] + matrix[j][i]
+    for i in matrix:
+        print('|', end='')
+        for j in i:
+            print(f'{j:5}', end='')
+        print(' |')
