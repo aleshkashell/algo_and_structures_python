b0243eb73080a51866df8942909a44ea0c9adc6e
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-08-10 20:03:43 +0300

    second task done

--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,16 +3,15 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
-import cProfile
 from timeit import timeit
 import math
 
 
-def get_primes(count):
+def get_primes(index):
     primes = [2]
     i = 1
     while True:
-        if len(primes) >= count:
+        if len(primes) >= index:
             break
         i = i + 2
         if (i > 10) and (i%10 == 5):
@@ -21,28 +20,58 @@ def get_primes(count):
             if j * j - 1 > i:
                 primes.append(i)
                 break
-            if (i % j == 0):
+            if i % j == 0:
                 break
         else:
             primes.append(i)
-    return primes
+    return primes[len(primes)-1]
 
 
-def primes_sieve(limit):
-    a = [True] * int(limit)                          # Initialize the primality list
+def primes_sieve(index):
+    if index == 1:
+        return 2
+    if index < 7:
+        coef = 3
+    else:
+        coef = 1.4
+    limit = int(index * math.log(index) * coef)
+    a = [True] * limit                          # Initialize the primality list
     a[0] = a[1] = False
     primes = list()
 
     for (i, isprime) in enumerate(a):
         if isprime:
             primes.append(i)
-            for n in range(i*i, int(limit), i):     # Mark factors non-prime
+            for n in range(i*i, limit, i):     # Mark factors non-prime
                 a[n] = False
-    return primes
+    # print(primes)
+    return primes[index-1]
 
 
 if __name__ == '__main__':
-    n = 500
-    border = n * math.log(n) * 1.4
-    print(get_primes(n))
-    print(primes_sieve(border))
+    number = 10000
+    n = 11
+    print("Время выполнения обычной функции: ", end='')
+    print(timeit(f'get_primes({n})', setup='from __main__ import get_primes', number=number))
+    print("Время выполнения решета Эратосфена: ", end='')
+    print(timeit(f'primes_sieve({n})', setup='from __main__ import primes_sieve', number=number))
+
+"""
+Нахождение 10-го числа (1000000 запусков):
+Время выполнения обычной функции: 7.015490589001274
+Время выполнения решета Эратосфена: 7.9177059270004975
+
+Нахождение 100-го числа (1000 запусков):
+Время выполнения обычной функции: 1.7657503859954886
+Время выполнения решета Эратосфена: 1.1618186120031169
+
+Нахождение 1000000-го числа:
+Время выполнения обычной функции: 76.8135189780005
+Время выполнения решета Эратосфена: 4.909552088000055
+
+
+Сложность решета Эратосфена O(n log(n)), второго алгоритма - O(n).
+Из результатов можно сделать вывод, что алгоритм сложностью O(n log(n)) гораздо эффективнее с увеличением количетва элементов,
+а в данном примере преимущество решета Эратосфена проявляется начиная с 11 элементов.
+
+"""
