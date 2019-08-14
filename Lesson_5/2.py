5c1baa726e4a415444f8dcc940d4383c5b1a1e13
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-08-14 21:16:04 +0300

    sum realisation done

--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,31 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+from collections import deque
+from itertools import zip_longest
+
+
+class HexNum:
+    def __init__(self, text):
+        self.data = deque([i for i in text])
+
+    def __add__(self, other):
+        result = deque()
+        addit = 0
+        for a, b in zip_longest(reversed(self.data), reversed(other.data), fillvalue='0'):
+            print(f'a={a}, b={b}')
+            digit_sum = int(a, 16) + int(b, 16) + addit
+            result.appendleft(f"{digit_sum % 16:X}")
+            addit = digit_sum // 16
+        if addit > 0:
+            result.appendleft(f"{addit % 16:X}")
+        return result
+
+    def __str__(self):
+        return "".join(self.data)
+
+
+if __name__ == '__main__':
+    data1 = HexNum('ffff')
+    data2 = HexNum('ffff')
+    print(data1 + data2)
