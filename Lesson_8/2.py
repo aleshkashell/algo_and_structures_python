c8efb345859ed8dd41f9e6fdd81a6c85ea1eba6f
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-08-23 18:04:22 +0300

    second task done

--- Lesson_8/2.py
+++ Lesson_8/2.py
@@ -3,3 +3,21 @@
 Пусть дана строка S длиной N, состоящая только из маленьких латинских букв.
 Требуется найти количество различных подстрок в этой строке.
 """
+import hashlib
+
+
+def main():
+    substrings = set()
+    text = input("Введите строку из маленьких латинских букв: ")
+    length = len(text)
+    for i in range(length):
+        for j in range(i, length):
+            if j + 1 - i < length:
+                substrings.add(hashlib.md5(text[i:j+1].encode('utf-8')).hexdigest())
+
+    print(f'В строке "{text}" {len(substrings)} подстрок')
+    print(substrings)
+
+
+if __name__ == '__main__':
+    main()
