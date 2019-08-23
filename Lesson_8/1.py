ea4bfaf097842b489caef5089c1e8932d7fae72d
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-08-23 17:28:49 +0300

    first task done

--- Lesson_8/1.py
+++ Lesson_8/1.py
@@ -1,3 +1,48 @@
 """
 1. Закодируйте любую строку из трех слов по алгоритму Хаффмана.
 """
+import heapq
+from collections import Counter
+from collections import namedtuple
+
+
+class Node(namedtuple("Node", ["left", "right"])):
+    def walk(self, code, acc):
+        self.left.walk(code, acc + "0")
+        self.right.walk(code, acc + "1")
+
+
+class Leaf(namedtuple("Leaf", ["char"])):
+    def walk(self, code, acc):
+        code[self.char] = acc or "0"
+
+
+def huffman_encode(s):
+    h = []
+    for ch, freq in Counter(s).items():
+        h.append((freq, len(h), Leaf(ch)))
+    heapq.heapify(h)
+    count = len(h)
+    while len(h) > 1:
+        freq1, _count1, left = heapq.heappop(h)
+        freq2, _count2, right = heapq.heappop(h)
+        heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
+        count += 1
+    code = {}
+    if h:
+        [(_freq, _count, root)] = h
+        root.walk(code, "")
+    return code
+
+
+def main():
+    s = input("Введите строку: ")
+    code = huffman_encode(s)
+    print("Закодированные символы:")
+    for ch in code:
+        print("'{}': {}".format(ch, code[ch]))
+    print(f"Результат: {' '.join(code[ch] for ch in s)}")
+
+
+if __name__ == "__main__":
+    main()
